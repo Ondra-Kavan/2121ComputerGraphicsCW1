94b1af04344cfcdec36ab6a4f39dd2c12e978ae4
Ondra-Kavan <ondrasik007@gmail.com>
2021-12-06 20:16:12 +0000

    initial commit

---
+++ CW/main.cpp
@@ -0,0 +1,21 @@
+#include <FreeImage/FreeImagePlus.h> 
+#include <wincodec.h> 
+#include <glew/glew.h> 
+#include <freeglut/freeglut.h> 
+#include <CoreStructures/CoreStructures.h> 
+#include <iostream>
+
+void init();
+void draw();
+
+void main(void) {
+	std::cout << "Hello, World!" << std::endl;
+}
+
+void init()
+{
+}
+
+void draw()
+{
+}
